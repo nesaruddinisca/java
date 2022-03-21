76aed00031121ffb9c72d5b20198eedc543e1b8d
nesaruddinisca <nesaruddinisca1992@gmail.com>
2022-03-21 14:51:39 +0600

    Person

---
+++ src/com/company/Teacher.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Teacher extends Person {
+    String Qualification;
+
+    void displayInformation2(){
+        displayInformation1();
+        System.out.println("Qualification :"+Qualification);
+
+
+    }
+}
---
+++ src/com/company/Test.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Teacher t1 = new Teacher();
+
+        t1.name = "Nesar Uddin";
+        t1.age = 25;
+        t1.Qualification = "Msc";
+        t1.displayInformation2();
+
+        t1.name = "Borhan Uddin";
+        t1.age = 26;
+        t1.Qualification = "Msc";
+        t1.displayInformation2();
+
+        t1.name = "Ismail hossen";
+        t1.age = 30;
+        t1.Qualification = "Msc";
+        t1.displayInformation2();
+
+
+    }
+}
